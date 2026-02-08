7d09489f87309c367fe6b2a9f5592ab390373c74
sobbingtofu <kukubasak@naver.com>
2026-02-09 06:06:54 +0900

    Feat : 별칭 입력 외부 클릭 해제 기능 및 별칭 글자수 제한 기능 구현

--- src/widgets/favorite-list/ui/favorite-card.tsx
+++ src/widgets/favorite-list/ui/favorite-card.tsx
@@ -2,7 +2,7 @@ import {TimeThemeStyles} from "@/entities/date-time";
 import {FavoriteLocation} from "@/features/manage-favorites";
 import {MapPin, Pen, Trash2} from "lucide-react";
 import {useRouter} from "next/navigation";
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 
 interface FavoriteCardProps {
   item: FavoriteLocation;
@@ -13,8 +13,24 @@ interface FavoriteCardProps {
 
 function FavoriteCard({item, onRemove, onUpdate, themeStyles}: FavoriteCardProps) {
   const router = useRouter();
+  const favoriteCardRef = useRef<HTMLDivElement>(null);
   const [isEditing, setIsEditing] = useState(false);
   const [editValue, setEditValue] = useState(item.nickName || item.address);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (favoriteCardRef.current && !favoriteCardRef.current.contains(event.target as Node)) {
+        setIsEditing(false);
+      }
+    };
+
+    if (isEditing) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isEditing]);
 
   const handleSave = (e: React.MouseEvent) => {
     e.stopPropagation();
@@ -42,18 +58,39 @@ function FavoriteCard({item, onRemove, onUpdate, themeStyles}: FavoriteCardProps
 
   return (
     <div
+      ref={favoriteCardRef}
       onClick={handleCardClick}
       className={`h-[120px] rounded-lg p-5 hover:shadow-md transition-shadow cursor-pointer bg-white flex justify-between items-center group ${themeStyles.cardBg}`}
     >
       <div className="flex flex-col gap-1">
         {isEditing ? (
           <div className="flex gap-2 h-[50px] py-2" onClick={(e) => e.stopPropagation()}>
-            <input
-              className="border py-1 px-2 rounded text-sm"
-              value={editValue}
-              onChange={(e) => setEditValue(e.target.value)}
-            />
-            <button onClick={handleSave} className="text-xs bg-blue-500 text-white px-4 py-1 rounded">
+            <div className="relative flex-1 h-full flex items-center">
+              {/* 글자수 표시 */}
+              <span
+                className={`absolute right-0 -top-[18px] text-[11px] ${
+                  editValue.length >= 60 ? "text-red-500" : "text-gray-400"
+                }`}
+              >
+                {editValue.length}/60
+              </span>
+              {/* 별칭입력창 */}
+              <input
+                className="w-full h-full border border-gray-400 focus:outline-gray-400 focus:ring-1 py-1 px-2
+                rounded text-sm"
+                value={editValue}
+                onChange={(e) => {
+                  if (e.target.value.length <= 60) {
+                    setEditValue(e.target.value);
+                  }
+                }}
+              />
+            </div>
+            <button
+              onClick={handleSave}
+              className="text-xs bg-blue-500 hover:bg-blue-400 text-white px-4 py-1 rounded
+              transition-colors duration-200 ease-in-out"
+            >
               저장
             </button>
           </div>
@@ -72,11 +109,11 @@ function FavoriteCard({item, onRemove, onUpdate, themeStyles}: FavoriteCardProps
       <div className="flex gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
         <button
           onClick={handleEditClick}
-          className="p-2 text-gray-400 hover:text-blue-500 hover:bg-blue-50 rounded-full"
+          className="p-3 text-gray-400 hover:text-blue-500 hover:bg-blue-50 rounded-full"
         >
           <Pen size={16} />
         </button>
-        <button onClick={handleDelete} className="p-2 text-gray-400 hover:text-red-500 hover:bg-red-50 rounded-full">
+        <button onClick={handleDelete} className="p-3 text-gray-400 hover:text-red-500 hover:bg-red-50 rounded-full">
           <Trash2 size={16} />
         </button>
       </div>
